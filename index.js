b2984469323310377bf2190c8dba893f04181de0
Blair Stuart <blaircstuart28@gmail.com>
2018-09-13 17:21:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function happyHolidays(){
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
-function happyHolidayTo(holiday, name){
+function happyHolidayTo(name, holiday){
   return `Happy ${holiday}, ${name}`
 }
 function holidayCountdown(holiday, days){
